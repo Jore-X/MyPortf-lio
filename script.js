a3abac1e0ae0b631e675240e02825d8a89234332
Djore_017 <138606159+Jore-X@users.noreply.github.com>
2026-04-16 18:26:38 -0300

    Versão 1 incompleta
    versão com Inicio, e com pagina de projetos

---
+++ script.js
@@ -0,0 +1,10 @@
+function trocar(id) {
+  document.querySelectorAll(".sections").forEach((secoes) => {
+    secoes.style.display = "none";
+  });
+  document.getElementById(id).style.display = "flex";
+}
+
+window.onload = () => {
+    trocar('home');
+};
