aa27b19f26abda8f94c16ebad96af1e19b7a9c9e
Djore_017 <138606159+Jore-X@users.noreply.github.com>
2026-07-17 15:30:14 -0300

    finalizada
    - funcionamento header e side menu 100%
    - responsividade 100% tambem

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const about_card = document.querySelector(".about-card");
 const about_overlay = document.querySelector(".about-overlay");
 const about_overlay_p = document.querySelector(".about-overlay > p");
+const open_side_btn = document.querySelector(".open-btn");
+const close_side_btn = document.querySelector(".close-btn");
+const close_side_shadow = document.querySelector(".side-shadow-close");
+const side_menu = document.querySelector(".side-menu");
 
 about_overlay.addEventListener("click", () => {
   about_card.classList.toggle("active");
@@ -10,3 +14,18 @@ about_overlay.addEventListener("click", () => {
     about_overlay.textContent = "Ver mais";
   }
 });
+
+open_side_btn.addEventListener("click", () => {
+  side_menu.classList.add("active");
+  close_side_shadow.classList.add("active");
+});
+close_side_btn.addEventListener("click", () => {
+  side_menu.classList.remove("active");
+  close_side_shadow.classList.remove("active");
+});
+close_side_shadow.addEventListener("click", () => {
+  side_menu.classList.remove("active");
+  close_side_shadow.classList.remove("active");
+});
+
+
--- scripts/toggleNav.js
+++ scripts/toggleNav.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   const sections = document.querySelectorAll(".sections"); // ajuste o seletor se necessário
   const navLinks = document.querySelectorAll(".navLinks"); // ajuste o seletor conforme seu HTML
+  const navSideLinks = document.querySelectorAll(".nav-aside-links"); // ajuste o seletor conforme seu HTML
   const header = document.querySelector("header");
-  
+
   window.addEventListener("scroll", function () {
     const headerHeight = header ? header.offsetHeight : 0;
     const scrollPosition = window.scrollY + headerHeight;
@@ -24,12 +25,14 @@ document.addEventListener("DOMContentLoaded", function () {
       activeSectionIndex = sections.length - 1;
     }
 
-    // Remove a classe 'active' de todos os links
-
+    
     // Adiciona a classe 'active' no link correspondente
     if (navLinks[activeSectionIndex]) {
+      // Remove a classe 'active' de todos os links
       navLinks.forEach((link) => link.classList.remove("active"));
+      navSideLinks.forEach((link) => link.classList.remove("active"));
       navLinks[activeSectionIndex].classList.add("active");
+      navSideLinks[activeSectionIndex].classList.add("active");
     }
   });
 });
